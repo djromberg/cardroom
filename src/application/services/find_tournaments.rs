e4006c7b23b442be090eef08071e1a72b5064264
Daniel Romberg <daniel@romberg.io>
2026-03-10 22:13:08 +0100

    implement filtering and forward stage

--- src/application/services/find_tournaments.rs
+++ src/application/services/find_tournaments.rs
@@ -5,6 +5,7 @@ use crate::application::AuthRole;
 use crate::domain::QueryTournaments;
 use crate::domain::QueryTournamentsError;
 use crate::domain::Tournament;
+use crate::domain::TournamentStage;
 
 use serde::Deserialize;
 use serde::Serialize;
@@ -38,7 +39,7 @@ pub struct TournamentSummary {
     pub table_seat_count: u8,
     pub player_count: u32,
     pub creation_date: String,
-    pub phase: String,
+    pub stage: String,
     pub player_is_involved: bool,
 }
 
@@ -57,22 +58,29 @@ pub(in crate::application) fn find_tournaments<Repository: QueryTournaments>(
     repository: &Repository,
 ) -> Result<FindTournamentsResponse, FindTournamentsError> {
     let account_id = auth_info.expect_role(AuthRole::Observer)?;
-
     let tournaments = repository.query_tournaments()?;
-
-    let summaries = tournaments.iter().map(|tournament| get_tournament_summary(tournament, account_id)).collect();
-
+    let summaries = tournaments.iter().filter(
+        |tournament|
+            request.min_table_count.is_none_or(|min_table_count| tournament.table_count() >= min_table_count as usize) &&
+            request.max_table_count.is_none_or(|max_table_count| tournament.table_count() <= max_table_count as usize)
+    ).map(|tournament| get_tournament_summary(tournament, account_id)).collect();
     Ok(summaries)
 }
 
 
 pub(in crate::application) fn get_tournament_summary(tournament: &Tournament, account_id: Uuid) -> TournamentSummary {
+    // TODO: make this more elegant without duplicating/exposing domain value TournamentStage
+    let stage = match tournament.stage() {
+        TournamentStage::WaitingForPlayers => "WaitingForPlayers",
+        TournamentStage::Running => "Running",
+        TournamentStage::Finished => "Finished",
+    };
     TournamentSummary {
         tournament_id: tournament.id().to_string(),
         table_count: tournament.table_count() as u16,
         table_seat_count: tournament.table_seat_count(),
         player_count: tournament.player_count() as u32,
-        phase: "WaitingForPlayers".to_string(),
+        stage: stage.to_string(),
         creation_date: "2024-08-01T14:38:32.499588".to_string(),
         player_is_involved: tournament.has_player(account_id),
         seat_count: 23,
